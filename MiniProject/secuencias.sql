58a13d737f66dd70a0317fa71d60e7455fa4222e
garzuxo <garzuxo@gmail.com>
2018-10-01 21:45:21 -0500

    sequences

---
+++ MiniProject/secuencias.sql
@@ -0,0 +1,21 @@
+﻿create sequence tmio1_buses_sec
+start with 1
+increment by 1
+cycle;
+
+create sequence tmio1_sitios_sec
+start with 1
+increment by 1
+cycle;
+
+create sequence tmio1_rutas_sec
+start with 1
+increment by 1
+cycle;
+
+
+
+
+
+
+  
